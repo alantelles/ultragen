8649b275b4da5f77b939e0b3980955c4df6c52c4
Alan Telles <alantelles86@gmail.com>
2020-06-16 01:59:11 -0300

    define semantic analyzer methods

--- src/Units/symboltablecreatorclass.pas
+++ src/Units/symboltablecreatorclass.pas
@@ -7,6 +7,8 @@ interface
 uses
       Classes, SysUtils, ASTClass,
       BinOpClass, NumClass, UnaryOpClass, BinLogicOpClass, UnaryLogicopClass,
+      FlowControlASTClass,
+
       Tokens, ImpParserClass, StrUtils, LoggingClass,
       SymbolTableClass, SymbolsClass, CoreFunctionsClass;
 
@@ -22,20 +24,23 @@ type
       property PCurrentScope:TSymbolTable read FCurrentScope;
       // constructor Create(AParser:TTParser);
       constructor Create(var ATree: TAST);
-      function Visit(ANode:TAST):Extended;
+      procedure Visit(ANode:TAST);
       procedure VisitProgram(ANode: TProgram);
       procedure VisitFunctionDefinition(ANode: TFunctionDefinition);
       procedure VisitNoOp(ANode: TNoOp);
       procedure VisitFunctionCall(ANode: TFunctionCall);
-      function VisitVarAssign(ANode: TVarAssign):extended;
+      procedure VisitVarAssign(ANode: TVarAssign);
       procedure VisitVariableReference(ANode: TVariableReference);
-      function VisitBinOp(ANode:TBinOp):Extended;
-      function VisitBinLogicOp(ANode: TBinLogicOp):boolean;
-      function VisitNum(ANode:TNumFloat):Extended;
-      function VisitUnaryOp(ANode: TUnaryOp):Extended;
-      function VisitUnaryLogicOp(ANode: TUnaryLogicOp):boolean;
-      function VisitString(ANode: TString):Extended;
-      function Interpret:string;
+
+      procedure VisitListAccess(ANode: TListAccessAST);
+      procedure VisitList(ANode: TListAST);
+      procedure VisitConditional(ANode: TConditional);
+      procedure VisitIfCondition(ANode: TIfConditionBlock);
+      procedure VisitForLoop(ANode:TForLoop);
+
+      procedure VisitWhileLoop(ANode: TWhileLoop);
+
+      procedure Interpret;
 
 	end;
 
@@ -57,14 +62,12 @@ begin
   Visit(ATree);
 end;
 
-function TSymbolTableBuilder.Interpret:string;
+procedure TSymbolTableBuilder.Interpret;
 var
   ATree:TAST;
   Ret:Extended;
 begin
   ATree := FParser.ParseCode;
-  Ret := Visit(ATree);
-  Result := FloatToStr(Ret);
 end;
 
 procedure TSymbolTableBuilder.VisitFunctionDefinition(ANode: TFunctionDefinition);
@@ -96,7 +99,7 @@ begin
   // ASymbolTable.AsString;
 end;
 
-function TSymbolTableBuilder.VisitVarAssign(ANode: TVarAssign):extended;
+procedure TSymbolTableBuilder.VisitVarAssign(ANode: TVarAssign);
 var
   ASolve: Extended;
   AVarSym: TVariableSymbol;
@@ -180,218 +183,59 @@ begin
 
 end;
 
-function TSymbolTableBuilder.Visit(ANode:TAST):Extended;
+procedure TSymbolTableBuilder.Visit(ANode:TAST);
 var
   AuxBool:boolean;
 begin
   if ANode.ClassNameIs('TProgram') then
   begin
     VisitProgram(TProgram(ANode));
-    Result := 1;
-  end
-  else if ANode.ClassNameIs('TString') then
-  begin
-    Result := VisitString(TString(ANode));
-  end
-  else if ANode.ClassNameIs('TFunctionCall') then
-  begin
-    VisitFunctionCall(TFunctionCall(ANode));
-    Result := 1;
-  end
-  else if Anode.ClassNameIs('TFunctionDefinition')  then
-  begin
-    VisitFunctionDefinition(TFunctionDefinition(ANode));
-    Result := 1;
   end
   else if ANode.ClassNameIs('TVarAssign') then
-    Result := VisitVarAssign(TVarAssign(ANode))
+    VisitVarAssign(TVarAssign(ANode))
   else if ANode.ClassNameIs('TVariableReference') then
-  begin
-    VisitVariableReference(TVariableReference(Anode));
-    Result := 1;
-	end;
-	{else if Anode.ClassNameIs('TNum') then
-    Result := VisitNum(TNumFloat(ANode))
-  else if ANode.ClassNameIs('TUnaryOp') then
-    Result := VisitUnaryOp(TUnaryOp(ANode))
-  else if ANode.ClassNameIs('TBinOp') then
-    Result := VisitBinOp(TBinOp(ANode))
-  else if ANode.ClassNameIs('TBinLogicOp') then
-  begin
-    AuxBool := VisitBinLogicOp(TBinLogicOp(ANode));
-    if AuxBool then
-      Result := 1
-    else
-      Result := 0;
-  end
-  else if ANode.ClassNameIs('TUnaryLogicOp') then
-  begin
-    AuxBool := VisitUnaryLogicOp(TUnaryLogicOp(ANode));
-    if AuxBool then
-      Result := 1
-    else
-      Result := 0;
-  end;}
+    VisitVariableReference(TVariableReference(ANode))
+  else if ANode.ClassNameIs('TFunctionCall') then
+    VisitFunctionCall(TFunctionCall(Anode))
+  else if Anode.ClassNameIs('TListAST') then
+    VisitList(TListAST(ANode))
+  else if Anode.ClassNameIs('TListAccessAST') then
+    VisitListAccess(TListAccessAST(ANode))
+  else if ANode.ClassNameIs('TConditional') then
+    VisitConditional(TConditional(ANode))
+  else if ANode.ClassNameIs('TIfConditionBlock') then
+    VisitIfCondition(TIfConditionBlock(ANode))
+  else if ANode.ClassNameIs('TWhileLoop') then
+    VisitWhileLoop(TWhileLoop(ANode))
+  else if ANode.ClassNameIs('TForLoop') then
+    VisitForLoop(TForLoop(ANode));
 end;
 
-function TSymbolTableBuilder.VisitString(ANode: TString):Extended;
+procedure TSymbolTableBuilder.VisitListAccess(ANode: TListAccessAST);
 begin
-  logdebug('Visiting string node', 'SymbolTableClass');
-  Result := 1;
+
 end;
 
-function TSymbolTableBuilder.VisitUnaryOp(ANode: TUnaryOp):Extended;
-var
-  AOper:string;
+procedure TSymbolTableBuilder.VisitList(ANode: TListAST);
 begin
 
-  AOper := ANode.POper.PType;
-  if AOper = T_PLUS then
-    Result := Visit(ANode.PExpr)
-  else if AOper = T_MINUS then
-    Result := Visit(ANode.PExpr) * (-1);
+end;
+procedure TSymbolTableBuilder.VisitConditional(ANode: TConditional);
+begin
+
+end;
+procedure TSymbolTableBuilder.VisitIfCondition(ANode: TIfConditionBlock);
+begin
+
+end;
+procedure TSymbolTableBuilder.VisitForLoop(ANode:TForLoop);
+begin
+
 end;
 
-function TSymbolTableBuilder.VisitUnaryLogicOp(ANode: TUnaryLogicOp):boolean;
-var
-  AOper:string;
-  AuxExt:Extended;
+procedure TSymbolTableBuilder.VisitWhileLoop(ANode: TWhileLoop);
 begin
 
-  AuxExt := Visit(ANode.PExpr) * (-1);
-  if AuxExt >= 0 then
-    Result := True
-  else
-    Result := False;
-end;
-
-function TSymbolTableBuilder.VisitBinLogicOp(ANode: TBinLogicOp): boolean;
-var
-  LeftRes, RightRes:Extended;
-  AuxBool, LeftBool, RightBool:boolean;
-begin
-
-  if Anode.ClassNameIs('TNumFloat') then
-    LeftRes := VisitNum(TNumFloat(ANode))
-  else if ANode.PLeft.ClassNameIs('TUnaryOp') then
-    LeftRes := VisitUnaryOp(TUnaryOp(ANode.PLeft))
-  else if ANode.PLeft.ClassNameIs('TBinOp') then
-    LeftRes := VisitBinOp(TBinOp(ANode.PLeft))
-  else if ANode.PLeft.ClassNameIs('TBinLogicOp') then
-  begin
-    AuxBool := VisitBinLogicOp(TBinLogicOp(ANode.PLeft));
-    if AuxBool then
-      LeftRes := 1
-    else
-      LeftRes := 0;
-  end
-  else if ANode.PLeft.ClassNameIs('TUnaryLogicOp') then
-  begin
-    AuxBool := VisitUnaryLogicOp(TUnaryLogicOp(ANode.PLeft));
-    if AuxBool then
-      LeftRes := 1
-    else
-      LeftRes := 0;
-  end;
-
-  if ANode.PRight.ClassNameIs('TNum') then
-    RightRes := VisitNum(TNumFloat(ANode.PRight))
-  else if ANode.PRight.ClassNameIs('TUnaryOp') then
-    RightRes := VisitUnaryOp(TUnaryOp(ANode.PRight))
-  else if ANode.PRight.ClassNameIs('TBinOp') then
-    RightRes := VisitBinOp(TBinOp(ANode.PRight))
-  else if ANode.PRight.ClassNameIs('TBinLogicOp') then
-  begin
-    AuxBool := VisitBinLogicOp(TBinLogicOp(ANode.PRight));
-    if AuxBool then
-      RightRes := 1
-    else
-      RightRes := 0;
-  end
-  else if ANode.PRight.ClassNameIs('TUnaryLogicOp') then
-  begin
-    AuxBool := VisitUnaryLogicOp(TUnaryLogicOp(ANode.PRight));
-    if AuxBool then
-      RightRes := 1
-    else
-      RightRes := 0;
-  end;
-
-  LeftBool := LeftRes > 0;
-  RightBool := RightRes > 0;
-
-  if (ANode.POper.PType = T_GT) then
-    Result := LeftRes > RightRes
-  else if (ANode.POper.PType = T_LT) then
-    Result := LeftRes < RightRes
-  else if (ANode.POper.PType = T_EQ) then
-    Result := LeftRes = RightRes
-  else if (ANode.POper.PType = T_LEQ) then
-    Result := LeftRes <= RightRes
-  else if (ANode.POper.PType = T_GEQ) then
-    Result := LeftRes >= RightRes
-  else if (ANode.POper.PType = T_NEQ) then
-    Result := LeftRes <> RightRes
-  else if (ANode.POper.PType = T_AND) then
-    Result := LeftBool and RightBool
-  else if (ANode.POper.PType = T_OR) then
-    Result := LeftBool or RightBool;
-end;
-
-function TSymbolTableBuilder.VisitBinOp(ANode:TBinOp): Extended;
-var
-  LeftRes, RightRes:Extended;
-  AuxBool: boolean;
-begin
-
-  if ANode.PLeft.ClassNameIs('TNum') then
-    LeftRes := VisitNum(TNumFloat(ANode.PLeft))
-  else if ANode.PLeft.ClassNameIs('TUnaryOp') then
-    LeftRes := VisitUnaryOp(TUnaryOp(ANode.PLeft))
-  else if ANode.PLeft.ClassNameIs('TBinOp') then
-    LeftRes := VisitBinOp(TBinOp(ANode.PLeft))
-  else if ANode.PLeft.ClassNameIs('TBinLogicOp') then
-  begin
-    AuxBool := VisitBinLogicOp(TBinLogicOp(ANode.PLeft));
-    if AuxBool then
-      LeftRes := 1
-    else
-      LeftRes := 0;
-  end;
-
-  if ANode.PRight.ClassNameIs('TNum') then
-    RightRes := VisitNum(TNumFloat(ANode.PRight))
-  else if ANode.PRight.ClassNameIs('TUnaryOp') then
-    RightRes := VisitUnaryOp(TUnaryOp(ANode.PRight))
-  else if ANode.PRight.ClassNameIs('TBinOp') then
-    RightRes := VisitBinOp(TBinOp(ANode.PRight))
-  else if ANode.PRight.ClassNameIs('TBinLogicOp') then
-  begin
-    AuxBool := VisitBinLogicOp(TBinLogicOp(ANode.PRight));
-    if AuxBool then
-      RightRes := 1
-    else
-      RightRes := 0;
-  end;
-
-  if (ANode.POper.PType = T_PLUS) then
-    Result := LeftRes + RightRes
-  else if (ANode.POper.PType = T_MINUS) then
-    Result := LeftRes - RightRes
-  else if (ANode.POper.PType = T_MULT) then
-    Result := LeftRes * RightRes
-  else if (ANode.POper.PType = T_INT_DIV) then
-    Result := Floor(LeftRes / RightRes)
-  else if (ANode.POper.PType = T_MODULUS) then
-    Result := Floor(LeftRes) mod Floor(RightRes)
-  else if (ANode.POper.PType = T_DIV) then
-    Result := LeftRes / RightRes;
-end;
-
-function TSymbolTableBuilder.VisitNum(ANode:TNumFloat):Extended;
-begin
-
-  Result := ANode.PValue.ToExtended;
 end;
 
 
