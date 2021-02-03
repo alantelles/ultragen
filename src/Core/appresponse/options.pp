94077b9689530c1b6b2c16f5a186db9ead72d823
Alan Telles <alantelles86@gmail.com>
2021-02-02 23:28:32 -0300

    update example with includ Server and AppResponse

--- src/Core/appresponse/options.pp
+++ src/Core/appresponse/options.pp
@@ -4,8 +4,11 @@ begin
   begin
     FInter.PResponse.Location := TStringInstance(FParams[0]).PValue;
     FInter.PResponse.Code := 303;
+    FInter.PResponse.CodeText := 'Redirecting to ' + FInter.PResponse.Location;
     if Length(FParams) > 1 then
-      FInter.PResponse.Code := TIntegerInstance(FParams[1]).PValue;
+      FInter.PResponse.CodeText := TStringInstance(FParams[1]).PValue;
+    if Length(FParams) > 2 then
+      FInter.PResponse.Code := TIntegerInstance(FParams[2]).PValue;
   end
   else if FName = 'setStatusText' then
     Finter.PResponse.CodeText := TStringInstance(FParams[0]).PValue;
