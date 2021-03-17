dc7e38b098a4fc3011b51467bae3e971a0a3adbb
Alan Telles <alantelles86@gmail.com>
2021-03-17 09:43:12 -0300

    implement unset cookie

--- src/Core/cookies/options.pp
+++ src/Core/cookies/options.pp
@@ -7,6 +7,17 @@ begin
     ACookie.Name := TStringInstance(FParams[0]).PValue;
     ACookie.Value := FParams[1].AsString;
   end
+  else if FName = 'unset' then
+  begin
+    ACookie := Finter.PResponse.Cookies.Add;
+    ACookie.Path := '/';
+    ACookie.Name := TStringInstance(FParams[0]).PValue;
+    try
+      ACookie.Expires := StrToDateTime('1-1-1970 0:0:0')
+    except on E: Exception do
+      FInter.RaiseException(E.Message, 'InternalApi');
+    end;
+  end
   else if FNAme = 'get' then
     Ret := TStringInstance.Create(FInter.PRequest.CookieFields.Values[TStringInstance(FParams[0]).PValue]);
 end
